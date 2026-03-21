1ab628690723a80fa924a8453ec35380d800f20a
Daniel Palme <daniel.palme@gmx.de>
2026-03-21 20:41:57 +0100

    Update dependencies

--- src/ReportGenerator.Core/Licensing/LicenseValidator.cs
+++ src/ReportGenerator.Core/Licensing/LicenseValidator.cs
@@ -52,6 +52,7 @@ namespace Palmmedia.ReportGenerator.Core.Licensing
             { Guid.Parse("2b1be47b-9785-4c22-a708-b58453e0a669"), new DateTime(2024, 11, 1) },
             { Guid.Parse("c2acc361-1b27-44a9-9e5d-841d8467dac0"), new DateTime(2025, 3, 1) },
             { Guid.Parse("c5e2eaca-8cb5-4636-967b-e4898afc547c"), new DateTime(2026, 3, 10) },
+            { Guid.Parse("6a84e4fb-ed63-4b2e-b397-f7e30800243c"), new DateTime(2026, 3, 20) },
         };
 
         /// <summary>
